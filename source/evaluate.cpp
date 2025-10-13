e2f1c9c8f4f34f70b468b94ef1896792622c9f00
tora <bmvdsfy@gmail.com>
2025-10-13 06:50:48 +0000

    評価に使う駒の評価値をK55の学習したものに合わせる

--- source/evaluate.cpp
+++ source/evaluate.cpp
@@ -37,6 +37,42 @@ int PieceValue[PIECE_NB] = {
     0,
 };
 
+int HavingPieceValue[PIECE_NB] = {
+    0,
+    HavingPawnValue,
+    0,
+    0,
+    HavingSilverValue,
+    HavingBishopValue,
+    HavingRookValue,
+    HavingGoldValue,
+    HavingKingValue,
+    HavingProPawnValue,
+    0,
+    0,
+    HavingProSilverValue,
+    HavingHorseValue,
+    HavingDragonValue,
+    0,
+
+    0,
+    -HavingPawnValue,
+    0,
+    0,
+    -HavingSilverValue,
+    -HavingBishopValue,
+    -HavingRookValue,
+    -HavingGoldValue,
+    -HavingKingValue,
+    -HavingProPawnValue,
+    0,
+    0,
+    -HavingProSilverValue,
+    -HavingHorseValue,
+    -HavingDragonValue,
+    0,
+};
+
 Value evaluate(const Position &pos) {
   Value score = VALUE_ZERO;
 
@@ -49,7 +85,7 @@ Value evaluate(const Position &pos) {
     for (Piece pc : {PAWN, SILVER, BISHOP, ROOK, GOLD}) {
       // 手駒の枚数を取得
       int cnt = hand_count(pos.hand_of(c), pc);
-      score += (c == BLACK ? 1 : -1) * Value(cnt * (PieceValue[pc]));
+      score += (c == BLACK ? 1 : -1) * Value(cnt * (HavingPieceValue[pc]));
     }
   // 手番側から見た評価値を返す
   return pos.side_to_move() == BLACK ? score : -score;
--- source/evaluate.h
+++ source/evaluate.h
@@ -6,22 +6,38 @@
 
 namespace Eval {
 // Apery(WCSC26)の駒割り
+// 評価値参考： http://minerva.cs.uec.ac.jp/~ito/entcog/contents/lecture/date/5-kakinoki.pdf
 enum {
   // 駒の評価値
-  PawnValue = 100,
-  SilverValue = 600,
-  GoldValue = 700,
-  BishopValue = 900,
-  RookValue = 1000,
-  ProPawnValue = 400,
-  ProSilverValue = 650,
-  HorseValue = 1100,
-  DragonValue = 1200,
+  PawnValue = 107,
+  SilverValue = 810,
+  GoldValue = 907,
+  BishopValue = 1291,
+  RookValue = 1670,
+  ProPawnValue = 895,
+  ProSilverValue = 933,
+  HorseValue = 1985,
+  DragonValue = 2408,
   KingValue = 15000,
+};
+
+enum {
+  // 持ち駒の評価値
+  HavingPawnValue = 152,
+  HavingSilverValue = 1110,
+  HavingGoldValue = 1260,
+  HavingBishopValue = 1464,
+  HavingRookValue = 1998,
+  HavingProPawnValue = 0,
+  HavingProSilverValue = 0,
+  HavingHorseValue = 0,
+  HavingDragonValue = 0,
+  HavingKingValue = 0,
 };
 
 // 駒の価値のテーブル(後手の駒は負の値)
 extern int PieceValue[PIECE_NB];
+extern int HavingPieceValue[PIECE_NB];
 
 Value evaluate(const Position &pos);
 } // namespace Eval
