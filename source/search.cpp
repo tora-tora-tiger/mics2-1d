0f7af616315a2458ce81dcaea7de3ae82c91339e
tora <bmvdsfy@gmail.com>
2025-11-16 09:54:37 +0000

    千日手修正　continueをなくす

--- source/search.cpp
+++ source/search.cpp
@@ -190,13 +190,12 @@ void Search::search(Position &pos) {
           Move move = rootMoves[i].pv[0];           // 合法手のi番目
           pos.do_move(move, si);                    // 局面を1手進める
           Value value = VALUE_NONE;
+          std::vector<Move> pv;
           // 千日手(5五将棋ルール)は種類ごとの評価値で返す
           const RepetitionState &repetitionState = pos.is_repetition(16);
-          std::vector<Move> pv;
           if (repetitionState != REPETITION_NONE) {
             value = draw_value(repetitionState, pos.side_to_move());
             pos.undo_move(move);
-            continue;
           } else {
             // 1手進めた状態で探索を行っているため、ply_from_rootは1
             value = (-1) * alphabeta_search(pos, pv, alpha, beta, depth-1, 1); // 指定深さで探索
